e7ecd7e2c2bc323e0d272aee633344611021bb3b
Muhammad Naufal Adli <naufaladli0406@gmail.com>
2024-02-20 13:28:45 +0700

    Initial Commit

---
+++ ColorSelection/ColorSelectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSelectionApp.swift
+//  ColorSelection
+//
+//  Created by Naufal Adli on 20/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorSelectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
