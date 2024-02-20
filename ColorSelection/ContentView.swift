f65a0e6d7b301a919442d9f3ac703e16cc8010a0
Muhammad Naufal Adli <naufaladli0406@gmail.com>
2024-02-20 17:12:48 +0700

    last update

--- ColorSelection/ContentView.swift
+++ ColorSelection/ContentView.swift
@@ -7,15 +7,46 @@
 
 import SwiftUI
 
+struct ColorM: Identifiable{
+    var id = UUID()
+    var color: Color
+}
+
 struct ContentView: View {
+    @State var colors: [ColorM] = [ ColorM(color: .cyan), ColorM(color: .red), ColorM(color: .green), ColorM(color: .yellow), ColorM(color: .brown),ColorM(color: .orange),ColorM(color: .mint)
+                                    ,ColorM(color: .pink), ColorM(color: .purple), ColorM(color: .blue)
+    ]
+    
+    @State var selectedC: Color = .cyan
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack{
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 100,height: 100)
+                .foregroundColor(selectedC)
+            
+            LazyHGrid(rows: Array(repeating: GridItem(), count: 2), content: {
+                ForEach(colors){ color in
+                    Circle()
+                        .frame(width: 40, height: 40)
+                        .foregroundStyle(color.color)
+                        .opacity(0.8)
+                        .scaleEffect(selectedC == color.color ? 0.7 : 1)
+                        .overlay{
+                            Circle()
+                                .stroke(lineWidth: 3)
+                                .foregroundColor(selectedC == color.color ? .white : .clear)
+                        }
+                        .onTapGesture {
+                            withAnimation{
+                                selectedC = color.color
+                            }
+                        }
+                }
+            }).frame(height: 100)
         }
         .padding()
+        .background(.black.opacity(0.8),in: RoundedRectangle(cornerRadius: 20))
     }
 }
 
